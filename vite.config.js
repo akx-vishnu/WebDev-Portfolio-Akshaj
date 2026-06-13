a156cb3b8389091f77f0df98cac93b1171732282
akx-vishnu <akx-vishnu@users.noreply.github.com>
2026-06-13 23:24:14 +0530

    Fix blank screen on build, remove ngrok configurations, add loading preloader, and revert laptop to original

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        allowedHosts: [
-        ],
-        host: true
+    build: {
+        chunkSizeWarningLimit: 1500
     }
 })
